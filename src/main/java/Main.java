d39a3ae03283f2ec57ad600d3bd586f3e453fcc3
Leandro425 <>
2021-12-07 13:42:29 +0100

    init task 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
+
+    public static int get_current_balance(Connection conn, int ACCID){
+        System.out.println("Get current balance from ACCID: " + ACCID);
+        int balance = -100;
+        String query = "SELECT balance FROM accounts WHERE accid = " + ACCID;
+        try(Statement stmt = conn.createStatement()){
+            ResultSet result = stmt.executeQuery(query);
+            while(result.next()) {
+                balance  = result.getInt("balance");
+            }
+        } catch (SQLException e) {
+            e.toString();
+        };
+        System.out.println("Balance: " + balance);
+        return balance;
+    }
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("[ip] [username] [password]");
@@ -15,6 +29,7 @@ public class Main {
         System.out.println("Connecting to database...");
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);) {
             System.out.println("Connected!");
+            get_current_balance(conn, 3);
 
         } catch (SQLException e) {
             e.printStackTrace();
