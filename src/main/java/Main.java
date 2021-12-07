9c14d3924de5c41d70416cbbeb1e5c28e4efe646
Your Name <you@example.com>
2021-12-07 04:43:10 -0800

    Analye-Tx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,26 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
 
 public class Main {
+
+    public static int AnalyseTx(Connection con, int delta){
+        try{
+            Statement stmt=con.createStatement();
+
+
+            ResultSet rs=stmt.executeQuery("SELECT count(*) from history where " +
+                    "delta ="+ delta );
+            while(rs.next())
+
+            delta = rs.getInt(1);
+            con.close();
+            return delta;
+        }catch(Exception e){ System.out.println(e);}
+
+        return 0;
+    }
+
+
     public static void main(String[] args) {
         if (args.length != 3) {
             System.out.println("[ip] [username] [password]");
@@ -15,6 +33,9 @@ public class Main {
         System.out.println("Connecting to database...");
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);) {
             System.out.println("Connected!");
+
+           System.out.println("Anzahl der Datebsätze mit Einzahlungsbetrag Delta: "
+                   +AnalyseTx(conn, 5));
 
         } catch (SQLException e) {
             e.printStackTrace();
