ea33e5340427450910138b283125dc6506bc9855
Paul Steinebach <paul70011@live.de>
2021-12-14 01:30:09 +0100

    first running version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -15,6 +17,12 @@ public class Main {
 
     // analyse statements
     static PreparedStatement analyseStmt;
+
+    static Random rand = new Random();
+
+    static int maxBRANCHID;
+    static int maxTELLERID;
+    static int maxACCID;
 
     public static int balance(int ACCID) throws SQLException {
         balanceStmt.setInt(1, ACCID);
@@ -75,28 +83,67 @@ public class Main {
         System.out.println("Prepared!");
     }
 
+    public static void runRandomTX() throws SQLException {
+        int x = rand.nextInt(100);
+        if (x < 35) {
+            int ACCID = rand.nextInt(maxACCID) + 1;
+            System.out.printf("Balance: ACCID(%d) = %d%n", ACCID, balance(ACCID));
+        } else if (x < 35 + 50) {
+            int ACCID = rand.nextInt(maxACCID) + 1;
+            int TELLERID = rand.nextInt(maxTELLERID) + 1;
+            int BRANCHID = rand.nextInt(maxBRANCHID) + 1;
+            int DELTA = rand.nextInt(10000) + 1;
+            System.out.printf("Deposit: ACCID(%d) TELLERID(%d) BRANCHID(%d) DELTA(%d) = %d%n",
+                    ACCID, TELLERID, BRANCHID, DELTA, deposit(ACCID, TELLERID, BRANCHID, DELTA));
+        } else {
+            int DELTA = rand.nextInt(10000) + 1;
+            System.out.printf("Analyse: DELTA(%d) = %d%n", DELTA, analyse(DELTA));
+        }
+    }
+
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Required arguments: [ip] [username] [password]");
+        if (args.length != 4) {
+            System.out.println("Required arguments: [ip] [username] [password] [n]");
             System.exit(1);
         }
 
+        // defining database connection parameters
         final String DB_URL = "jdbc:mariadb://" + args[0] + ":3306/benchmark?rewriteBatchedStatements=true";
         final String USER = args[1];
         final String PASS = args[2];
 
-        System.out.println("Connecting to database...");
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);) {
+        // defining limits for IDs
+        final int n = Integer.parseInt(args[3]);
+        maxBRANCHID = 1 * n;
+        maxTELLERID = 10 * n;
+        maxACCID = 100000 * n;
+
+        try {
+            System.out.println("Connecting to database...");
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             System.out.println("Connected!");
             prepareStatements(conn);
 
-            System.out.println(balance(3));
-            System.out.println(deposit(3, 1, 1, 700));
-            System.out.println("Anzahl der Datebsätze mit Einzahlungsbetrag Delta: "
-                    + analyse(700));
+            for (long stop = System.nanoTime() + TimeUnit.MINUTES.toNanos(4); stop > System.nanoTime();) {
+                runRandomTX();
+                TimeUnit.MILLISECONDS.sleep(50);
+            }
+            int txCount = 0;
+            for (long stop = System.nanoTime() + TimeUnit.MINUTES.toNanos(5); stop > System.nanoTime();) {
+                runRandomTX();
+                txCount++;
+                TimeUnit.MILLISECONDS.sleep(50);
+            }
+            for (long stop = System.nanoTime() + TimeUnit.MINUTES.toNanos(1); stop > System.nanoTime();) {
+                runRandomTX();
+                TimeUnit.MILLISECONDS.sleep(50);
+            }
+
+            System.out.printf("TX count: %d%n", txCount);
+            System.out.printf("Avg TX/s: %f%n", txCount / (5f * 60f));
 
             conn.close();
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
